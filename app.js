0571024282ef3ce3325b3480e6f13fb145b5c865
RS <36763319+ADN1945@users.noreply.github.com>
2018-11-24 21:44:46 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-//hallo
 var Application = {
 	initApplication : function() {
 		$(window).load('pageinit', '#page-one', function() {
@@ -6,15 +5,58 @@ var Application = {
 			Application.initShowBerita(country);
 		})
 		$('#country').change(function (){
-            var country = $('#country :selected').val();
+            var country = $("#country :selected").val();
             Application.initShowBerita(country);
       	})
-
-		$(document).on('click', '#detail-mhs', function (){
-            var nim = $(this).data('nimmhs');
-            var index = $(this).data('indexmhs');
-            Application.initShowDetailMhs(nim,index);
+		$(document).on('click', '#btnSearch', function (){
+            var query = $("#boxSearch").val();
+            var sort = $("#sort :selected").val();
+            Application.initShowSearchResult(query,sort);
       	})
+	},
+
+	initShowSearchResult : function(query,sort) {
+		$.ajax({
+			url: 'https://newsapi.org/v2/everything?' +
+				'q='+query+'&' +
+				'sortBy='+sort+'&' +
+				'apiKey=1231536b76ed409a9707dde28b99c895',
+			type: 'get',
+			beforeSend : function(){
+				$.mobile.loading('show', {
+					text : 'Please wait while retrieving data...',
+					textVisible : true
+				});
+			},
+			success : function(dataObject){
+				var konten = $("#list-berita");
+				if(konten.children().length != 0){
+					konten.empty();
+				}
+				if(konten.children().length == 0){
+					for(var i=0; i<dataObject.articles.length; i++){
+						if(dataObject.articles[i].description == null){
+							dataObject.articles[i].description = "";
+						}
+
+						var appendList = '<li><a href="'+ dataObject.articles[i].url +'"><h2>'+dataObject.articles[i].source.name+
+						'</h2><p><b>'+
+						dataObject.articles[i].title+
+						'<div><img class="ui-corner-all ui-overlay-shadow img-thumbnail img-fluid img-berita" src="'+
+						 dataObject.articles[i].urlToImage +
+						'"></div></b></p><p>'+dataObject.articles[i].description+'</p></li>';
+						$('#list-berita').append(appendList);
+						$('#list-berita').listview('refresh');
+					}
+				}
+				$(".img-berita").on("error",function(){
+					$(this).attr('src','image/news-default.jpeg');
+				});
+			},
+			complete : function() {
+				$.mobile.loading('hide');
+			}
+		});
 	},
 
 	initShowBerita : function(country) {
